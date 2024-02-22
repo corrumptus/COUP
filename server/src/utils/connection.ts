9889cd956c9ae2f70fb1082f5b894a7fd67e8167
corrumptus <lazarinilucas42@gmail.com>
2024-02-22 20:45:45 -0300

    utility: creating database connection

---
+++ server/src/utils/connection.ts
@@ -0,0 +1,17 @@
+import { createPool } from "mysql2";
+import { Pool } from "mysql2/typings/mysql/lib/Pool";
+
+let pool: Pool;
+
+try {
+    pool = createPool({
+        host: process.env.SQL_HOST,
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASSWORD,
+        database: process.env.SQL_DATABASE
+    });
+} catch (error) {
+    process.exit(1);
+}
+
+export default pool.promise();
