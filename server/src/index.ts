4e4523cc2294c18d67351e821e90314d634baf90
corrumptus <lazarinilucas42@gmail.com>
2024-02-22 20:56:35 -0300

    endpoint: adding login and sign up endpoints for user

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import http from 'http'
 import { Server, Socket } from 'socket.io';
+import User from './entity/User';
+import UserService from './service/UserService';
 
 require('dotenv').config();
 
@@ -10,6 +12,42 @@ const PORT: number = Number(process.env.PORT) || 5000;
 api.use(express.json());
 
 api.listen(PORT, () => console.log(`server started in ${PORT}`));
+
+api.post("/login", async (req, res) => {
+    try {
+        const user = User.fromUnknownObject(req.body);
+
+        const newToken = await UserService.login(user);
+
+        res.status(200).send({
+            token: newToken,
+            socketInfos: {
+                url: req.protocol + '://' + req.get('host') + req.originalUrl,
+                name: user.getName()
+            }
+        });
+    } catch (error) {
+        res.status(401).send({ error: (error as Error).message });
+    }
+});
+
+api.post("/signup", async (req, res) => {
+    try {
+        const user = User.fromUnknownObject(req.body);
+
+        const newToken = await UserService.signup(user);
+
+        res.status(200).send({
+            token: newToken,
+            socketInfos: {
+                url: req.protocol + '://' + req.get('host') + req.originalUrl,
+                name: user.getName()
+            }
+        });
+    } catch (error) {
+        res.status(401).send({ error: (error as Error).message });
+    }
+});
 
 const server: http.Server = http.createServer(api);
 const serverSocket: Server = new Server(server);
