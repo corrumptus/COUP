7765f568b4442f0a0c77e59987a57e3f7a57d2a8
corrumptus <lazarinilucas42@gmail.com>
2024-03-04 06:50:30 -0300

    creating login page

---
+++ client/app/login/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import Form from "../components/Form";
+
+export default function Login() {
+  const router = useRouter();
+
+  async function clickHandler(name: string, password: string) {
+    if (!name || name.trim() === "") {
+      alert("Error: Invalid username");
+      return;
+    }
+
+    if (!password || password.trim() === "") {
+      alert("Error: Invalid password");
+      return;
+    }
+
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      body: JSON.stringify({ name: name, password: password })
+    });
+
+    const infos = await response.json();
+
+    if (response.status !== 200)
+      alert("Error: " + infos.error);
+    else {
+      localStorage.setItem("coup-token", infos.token);
+      router.push("/");
+    }
+  }
+
+  return (
+    <div
+      style={{backgroundImage: "url(./login-page.jpeg)"}}
+      className="h-full bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center"
+    >
+      <Form
+        type="Entrar"
+        clickHandler={clickHandler}
+      />
+    </div>
+  )
+}
