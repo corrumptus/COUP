0bc55c81cc3053f37d2c1843bfe979a8c5060dc4
corrumptus <lazarinilucas42@gmail.com>
2024-03-18 00:15:04 -0300

    changing home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,25 +1,46 @@
 "use client"
 
 import { useRouter } from "next/navigation";
-import Header from "./components/Header";
+import Header from "./components/home/Header";
+import { useState } from "react";
+import RulesView from "./components/home/RulesView";
+import ServersView from "./components/home/LobbysView";
 
 export default function Home() {
   const router = useRouter();
+  const [ isServersVisible, setServersVisibility ] = useState(false);
+  const [ isRulesVisible, setRulesVisibility ] = useState(false);
 
   function openServers() {
-    
+    setRulesVisibility(false);
+    setServersVisibility(true);
+  }
+
+  function closeServers() {
+    setServersVisibility(false);
   }
 
   function openRules() {
+    setServersVisibility(false);
+    setRulesVisibility(true);
+  }
 
+  function closeRules() {
+    setRulesVisibility(false);
   }
 
   return (
     <div
-      className="h-full bg-[url(../public/home-coup-image.jpg)] bg-center bg-no-repeat bg-cover flex flex-col"
+      className="h-full bg-[url(../public/home-coup-image.webp)] bg-center bg-no-repeat bg-cover flex flex-col"
     >
       <Header />
-      <main className="grid content-center h-full justify-items-start gap-1 pl-2">
+      <main className="grid content-center h-full justify-items-start gap-1 pl-2.5 relative">
+        {isServersVisible &&
+          <ServersView closeView={closeServers}/>
+        }
+        {isRulesVisible &&
+          <RulesView closeView={closeRules}/>
+        }
         <button className="home_button" onClick={() => router.push("/jogar")}>Jogar</button>
         <button className="home_button" onClick={openServers}>Servidores</button>
         <button className="home_button" onClick={openRules}>Regras</button>
