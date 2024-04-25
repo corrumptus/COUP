096148af2c2296145c49a781998b51b6ce70f973
corrumptus <lazarinilucas42@gmail.com>
2024-04-25 19:41:41 -0300

    creating new configs to tailwind

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,11 +7,25 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "phone/2": "475px",
+      "pc": "600px",
+      "pc-2": "950px"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        "shrink": "shrink 3s linear"
+      },
+      keyframes: {
+        "shrink": {
+          "0%": { height: "100%" },
+          "100%": { height: "0%" }
+        }
       }
     }
   },
