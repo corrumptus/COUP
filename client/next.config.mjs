3f2ad35dcd0897ece5bd0a46d33613be83dc4ecd
corrumptus <lazarinilucas42@gmail.com>
2024-06-30 03:16:56 -0300

    changing the rewrites function to allow /jogar/[id] to exists

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     async rewrites() {
         return [
           {
+            source: "/jogar/:id",
+            destination: "/jogar/:id"
+          },
+          {
             source: '/:path*',
             destination: '/',
           },
