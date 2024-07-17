6c42c02df0e7e57693d685417c46906d7a7097cf
corrumptus <lazarinilucas42@gmail.com>
2024-07-17 18:43:05 -0300

    refactoring the code of completed action requeriments

--- client/utils/UIChanger.ts
+++ client/utils/UIChanger.ts
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Action, Card, ContextType, GameState, Player } from "@pages/GameView";
 import { ActionRequeriments, MenuTypes } from "@components/GameActionMenu";
-import { Config } from "@utils/socketAPI";
+import { Config, COUPSocket } from "@utils/socketAPI";
 import { getChoosableCards } from "@utils/utils";
 import { newToaster } from "@utils/Toasters";
 
@@ -53,108 +53,13 @@ function performUIChange(
     if (goTo === MenuTypes.CLOSED)
         return [ MenuTypes.CLOSED, {} ];
 
-    if (newRequeriments.action === Action.RENDA) {
-        //todo: socket.emit("renda");
-        return [ MenuTypes.CLOSED, {} ];
-    }
+    if (isActionEmitable(gameState.game.configs, newRequeriments, menuType)) {
+        emitAction(
+            // socket,
+            gameState.game.configs,
+            newRequeriments
+        );
 
-    if (newRequeriments.action === Action.AJUDA_EXTERNA) {
-        //todo: socket.emit("ajudaExterna");
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.TAXAR && menuType === MenuTypes.CARD_PICKING) {
-        // todo: socket.emit(
-        //     "taxar",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.choosedSelfCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.CORRUPCAO && menuType === MenuTypes.CARD_PICKING) {
-        // todo: socket.emit(
-        //     "corrupcao",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.choosedSelfCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.EXTORQUIR && menuType === MenuTypes.CARD_PICKING) {
-        // todo: socket.emit(
-        //     "extorquir",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.choosedSelfCard as number,
-        //     newRequeriments.target as string
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.GOLPE_ESTADO) {
-        // todo: socket.emit(
-        //     "golpeEstado",
-        //     newRequeriments.target as string,
-        //     newRequeriments.choosedTargetCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-    
-    if (newRequeriments.action === Action.ASSASSINAR && menuType === MenuTypes.CARD_PICKING) {
-        // todo: socket.emit(
-        //     "assassinar",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.target as string,
-        //     newRequeriments.choosedSelfCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.INVESTIGAR && menuType === MenuTypes.CARD_PICKING) {
-        // todo: socket.emit(
-        //     "investigar",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.choosedSelfCard as number,
-        //     newRequeriments.target as string,
-        //     newRequeriments.choosedTargetCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (
-        newRequeriments.action === Action.TROCAR &&
-        menuType === MenuTypes.CARD_PICKING &&
-        quantidadeTrocar(gameState.game.configs, newRequeriments.choosedCardType as Card) === 2
-    ) {
-        // todo: socket.emit(
-        //     "trocar",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.choosedSelfCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (
-        newRequeriments.action === Action.TROCAR &&
-        menuType === MenuTypes.CARD_PICKING_CHANGE &&
-        quantidadeTrocar(gameState.game.configs, newRequeriments.choosedCardType as Card) === 1
-    ) {
-        // todo: socket.emit(
-        //     "trocar",
-        //     newRequeriments.choosedCardType as Card,
-        //     newRequeriments.choosedSelfCard as number,
-        //     newRequeriments.choosedTargetCard as number
-        // );
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.TROCAR_PROPRIA_RELIGIAO) {
-        // todo: socket.emit("trocarPropriaReligiao");
-        return [ MenuTypes.CLOSED, {} ];
-    }
-
-    if (newRequeriments.action === Action.TROCAR_RELIGIAO_OUTRO) {
-        // todo: socket.emit("trocarReligiaoOutro", newRequeriments.target as string);
         return [ MenuTypes.CLOSED, {} ];
     }
 
@@ -350,4 +255,86 @@ function getRequestProblems(
         return "Este player não tem dinheiro suficiente para ser extorquido";
 
     return undefined;
+}
+
+function isActionEmitable(
+    configs: Config,
+    requeriments: ActionRequeriments,
+    menuType: MenuTypes
+): boolean {
+    if (requeriments.action === Action.RENDA)
+        return true;
+
+    if (requeriments.action === Action.AJUDA_EXTERNA)
+        return true;
+
+    if (requeriments.action === Action.TAXAR && menuType === MenuTypes.CARD_PICKING)
+        return true;
+
+    if (requeriments.action === Action.CORRUPCAO && menuType === MenuTypes.CARD_PICKING)
+        return true;
+
+    if (requeriments.action === Action.EXTORQUIR && menuType === MenuTypes.CARD_PICKING)
+        return true;
+
+    if (requeriments.action === Action.GOLPE_ESTADO)
+        return true;
+
+    if (requeriments.action === Action.ASSASSINAR && menuType === MenuTypes.CARD_PICKING)
+        return true;
+
+    if (requeriments.action === Action.INVESTIGAR && menuType === MenuTypes.CARD_PICKING)
+        return true;
+
+    if (
+        requeriments.action === Action.TROCAR &&
+        menuType === MenuTypes.CARD_PICKING &&
+        quantidadeTrocar(configs, requeriments.choosedCardType as Card) === 2
+    )
+        return true;
+
+    if (
+        requeriments.action === Action.TROCAR &&
+        menuType === MenuTypes.CARD_PICKING_CHANGE &&
+        quantidadeTrocar(configs, requeriments.choosedCardType as Card) === 1
+    )
+        return true;
+
+    if (requeriments.action === Action.TROCAR_PROPRIA_RELIGIAO)
+        return true;
+
+    if (requeriments.action === Action.TROCAR_RELIGIAO_OUTRO)
+        return true;
+
+    return false;
+}
+
+function emitAction(
+    // socket: COUPSocket,
+    configs: Config,
+    requeriments: ActionRequeriments
+) {
+    const infos = [
+        requeriments.action,
+        requeriments.choosedCardType,
+        requeriments.choosedSelfCard,
+        requeriments.target,
+        requeriments.choosedTargetCard
+    ]
+    .filter((info, i) => {
+        if (
+            i === 4 &&
+            requeriments.action === Action.TROCAR &&
+            configs.tiposCartas[
+                requeriments.choosedCardType as keyof typeof configs.tiposCartas
+            ].quantidadeTrocar === 2
+        )
+            return undefined;
+
+        return info;
+    })
+    .filter(info => info !== undefined);
+
+    //@ts-ignore
+    // socket.emit(...infos);
 }
