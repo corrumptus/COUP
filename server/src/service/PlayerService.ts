845b1db306a7fd016b38ad5fed4dbfcd4624f847
corrumptus <lazarinilucas42@gmail.com>
2024-07-25 23:10:30 -0300

    creating a new pool of user for the players that will enter in a lobby

--- server/src/service/PlayerService.ts
+++ server/src/service/PlayerService.ts
@@ -2,23 +2,43 @@ import Player from "../entity/player";
 import LobbyService from "./LobbyService";
 import Lobby from "../entity/Lobby";
 import { COUPSocket } from "../socket/socket";
+import UserService from "./UserService";
 
 export default class PlayerService {
     private static players: {
-        [socketId: string]:
-        {
-            player: Player | null,
+        [socketId: string]: {
+            player: Player,
             lobbyID: number
         }
     } = {};
 
+    private static waitingPlayers: {
+        [name: string]: {
+            player: Player,
+            lobbyID: number
+        }
+    } = {};
+
+    private static WAITING_TIMEOUT_MS = 300_000;
+
+    static addWaitingPlayer(name: string, lobbyID?: number) {
+        const newPlayer = new Player(name);
+
+        PlayerService.waitingPlayers[name] = {
+            player: newPlayer,
+            lobbyID: lobbyID !== undefined ?
+                lobbyID
+                :
+                LobbyService.enterNewLobby(newPlayer)
+        };
+
+        setTimeout(
+            () => delete PlayerService.waitingPlayers[name],
+            PlayerService.WAITING_TIMEOUT_MS
+        );
+    }
+
     static setListeners(socket: COUPSocket) {
-        socket.on("login", (name: string) => {
-            const newPlayer = new Player(name);
-
-            PlayerService.players[socket.id].player = newPlayer;
-        });
-
         socket.on("disconnect", () => {
             LobbyService.deletePlayer(PlayerService.players[socket.id]);
 
@@ -29,7 +49,13 @@ export default class PlayerService {
     }
 
     private static declare(socket: COUPSocket) {
-        PlayerService.players[socket.id] = { player: null, lobbyID: -1 };
+        UserService.getName(socket.handshake.auth.token)
+            .then(name => {
+                PlayerService.players[socket.id] =
+                    PlayerService.waitingPlayers[name as string];
+
+                delete PlayerService.waitingPlayers[name as string];
+            });
     }
 
     static isPlayerLogedIn(socketID: string): boolean {
