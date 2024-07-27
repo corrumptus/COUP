6fb22693e143b9e05d79fce0eb11956a50723eee
corrumptus <lazarinilucas42@gmail.com>
2024-07-26 22:33:48 -0300

    creating a new property that links players and lobbys for messaging

--- server/src/service/LobbyMessageService.ts
+++ server/src/service/LobbyMessageService.ts
@@ -1,3 +1,11 @@
+import Lobby from "../entity/Lobby";
+import { COUPSocket } from "../socket/socket";
+
 export default class LobbyMessageService {
-    
+    private static lobbys: {
+        [lobbyId: number]: {
+            lobby: Lobby,
+            players: { socket: COUPSocket, name: string }[]
+        }
+    } = {}
 }
