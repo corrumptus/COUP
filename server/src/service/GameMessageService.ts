4f4c57dbeb5df40e7a22acad3b7aa6af20f70494
corrumptus <lazarinilucas42@gmail.com>
2024-07-28 12:32:23 -0300

    creating the game state type

--- server/src/service/GameMessageService.ts
+++ server/src/service/GameMessageService.ts
@@ -1,4 +1,65 @@
+import Action from "../entity/Action";
+import CardType from "../entity/CardType";
+import Religion from "../entity/Religion";
+import Config from "../utils/Config";
 import MessageService from "./MessageService";
+
+export enum PlayerState {
+    WAITING_TURN = "waitingTurn",
+    THINKING = "thinking",
+    WAITING_REPLY = "waitingReply",
+    BEING_ATTACKED = "beingAttacked",
+    INVESTIGATING = "investigating",
+    BEING_BLOCKED = "beingBlocked",
+    NEED_TO_GOLPE_ESTADO = "needToGolpeEstado"
+}
+
+export type Player = {
+    name: string,
+    cards: { card: CardType | undefined, isDead: boolean }[],
+    money: number,
+    religion?: Religion,
+    state: PlayerState
+}
+
+export enum ContextType {
+    INVESTIGATING,
+    BEING_ATTACKED,
+    OBSERVING
+}
+
+export type GameState = {
+    player: Player,
+    game: {
+        players: Omit<Player, "state">[],
+        currentPlayer: string,
+        asylum: number,
+        configs: Config
+    },
+    context: {
+        type: ContextType.INVESTIGATING,
+        card: CardType,
+        target: string,
+        investigatedCard: CardType,
+        targetCard: number
+    } | {
+        type: ContextType.BEING_ATTACKED,
+        attacker: string,
+        action: Action,
+        card: CardType,
+        attackedCard?: number,
+        previousAction?: Action,
+        preBlockAction?: Action
+    } | {
+        type: ContextType.OBSERVING,
+        attacker: string,
+        action?: Action,
+        card?: CardType,
+        target?: string,
+        attackedCard?: number,
+        isInvesting: boolean
+    }
+}
 
 export default class GameMessageService extends MessageService {
 
