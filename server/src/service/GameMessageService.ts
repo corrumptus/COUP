ba0485e73446c39a8dbc75034efa20f5c74558e7
corrumptus <lazarinilucas42@gmail.com>
2024-07-28 12:34:54 -0300

    creating a method that sends the inicial game state when a game is inited

--- server/src/service/GameMessageService.ts
+++ server/src/service/GameMessageService.ts
@@ -1,5 +1,6 @@
 import Action from "../entity/Action";
 import CardType from "../entity/CardType";
+import Game from "../entity/Game";
 import Religion from "../entity/Religion";
 import Config from "../utils/Config";
 import MessageService from "./MessageService";
@@ -62,5 +63,17 @@ export type GameState = {
 }
 
 export default class GameMessageService extends MessageService {
+    static initGameState(lobbyId: number) {
+        const { lobby, players } = super.lobbys[lobbyId];
 
+        const game = lobby.getGame() as Game;
+
+        players.forEach(p =>
+            p.socket.emit("beginMatch", GameMessageService.calculateGameState(game, p.name))
+        );
+    }
+
+    private static calculateGameState(game: Game, playerName: string): GameState {
+
+    }
 }
