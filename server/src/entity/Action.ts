dfe9652efc862d03f2be68ff44fb0bf7cd645029
corrumptus <lazarinilucas42@gmail.com>
2024-07-30 19:05:24 -0300

    adding more actions and removing the existing auxiliar strucures

--- server/src/entity/Action.ts
+++ server/src/entity/Action.ts
@@ -1,68 +1,18 @@
 enum Action {
     RENDA,
     AJUDA_EXTERNA,
-    GOLPE_ESTADO,
     TAXAR,
-    ASSASSINAR,
+    CORRUPCAO,
     EXTORQUIR,
-    TROCAR,
+    ASSASSINAR,
     INVESTIGAR,
+    GOLPE_ESTADO,
+    TROCAR,
+    TROCAR_PROPRIA_RELIGIAO,
+    TROCAR_RELIGIAO_OUTRO,
     CONTESTAR,
-    BLOQUEAR
+    BLOQUEAR,
+    CONTINUAR
 }
-
-const postActions: { [key in Action]: Action[] } = {
-    [Action.RENDA]: [],
-
-    [Action.AJUDA_EXTERNA]: [
-        Action.BLOQUEAR
-    ],
-
-    [Action.GOLPE_ESTADO]: [],
-
-    [Action.TAXAR]: [
-        Action.CONTESTAR
-    ],
-
-    [Action.ASSASSINAR]: [
-        Action.CONTESTAR,
-        Action.BLOQUEAR
-    ],
-
-    [Action.EXTORQUIR]: [
-        Action.CONTESTAR,
-        Action.BLOQUEAR
-    ],
-
-    [Action.TROCAR]: [
-        Action.CONTESTAR
-    ],
-
-    [Action.INVESTIGAR]: [
-        Action.TROCAR,
-        Action.CONTESTAR
-    ],
-
-    [Action.CONTESTAR]: [],
-
-    [Action.BLOQUEAR]: [
-        Action.CONTESTAR
-    ]
-}
-
-Object.seal(postActions);
-
-const targetActions: Action[] = [
-    Action.ASSASSINAR,
-    Action.BLOQUEAR,
-    Action.CONTESTAR,
-    Action.EXTORQUIR,
-    Action.GOLPE_ESTADO,
-    Action.INVESTIGAR
-]
-
-Object.seal(targetActions);
-
-export { postActions, targetActions };
 
 export default Action;
