cb1ea22181e9dcfa375fc44a8902bdad16879258
corrumptus <lazarinilucas42@gmail.com>
2024-07-30 22:02:27 -0300

    changing the parameters of the make action

--- server/src/service/GameService.ts
+++ server/src/service/GameService.ts
@@ -1,26 +1,26 @@
 import PlayerService from "./PlayerService";
 import Action from "../entity/Action";
-import Player from "../entity/player";
-import Turn from "../entity/Turn";
 import Game from "../entity/Game";
 import { COUPSocket } from "../socket/socket";
+import CardType from "../entity/CardType";
+import Player from "../entity/player";
 
 export default class GameService {
     static setListeners(socket: COUPSocket) {
         socket.on("renda", () => {
-            GameService.makeAction(Action.RENDA, socket.id);
+            GameService.makeAction(socket.id, Action.RENDA);
         });
 
         socket.on("ajudaExterna", () => {
-            GameService.makeAction(Action.AJUDA_EXTERNA, socket.id);
+            GameService.makeAction(socket.id, Action.AJUDA_EXTERNA);
         });
 
         socket.on("taxar", (card, selfCard) => {
-            GameService.makeAction(Action.TAXAR, socket.id);
+            GameService.makeAction(socket.id, Action.TAXAR, card, selfCard);
         });
 
         socket.on("corrupcao", (card, selfCard) => {
-            GameService.makeAction(Action.TAXAR, socket.id);
+            GameService.makeAction(socket.id, Action.CORRUPCAO, card, selfCard);
         });
 
         socket.on("extorquir", (card, selfCard, targetName) => {
@@ -29,7 +29,7 @@ export default class GameService {
             if (target === null)
                 return;
 
-            GameService.makeAction(Action.EXTORQUIR, socket.id, target);
+            GameService.makeAction(socket.id, Action.EXTORQUIR, card, selfCard, target);
         });
 
         socket.on("assassinar", (card, selfCard, targetName, targetCard) => {
@@ -38,7 +38,7 @@ export default class GameService {
             if (target === null)
                 return;
 
-            GameService.makeAction(Action.ASSASSINAR, socket.id, target);
+            GameService.makeAction(socket.id, Action.ASSASSINAR, card, selfCard, target, targetCard);
         });
 
         socket.on("investigar", (card, selfCard, targetName, targetCard) => {
@@ -47,7 +47,7 @@ export default class GameService {
             if (target === null)
                 return;
 
-            GameService.makeAction(Action.INVESTIGAR, socket.id, target);
+            GameService.makeAction(socket.id, Action.INVESTIGAR, card, selfCard, target, targetCard);
         });
 
         socket.on("golpeEstado", (targetName, targetCard) => {
@@ -56,11 +56,11 @@ export default class GameService {
             if (target === null)
                 return;
 
-            GameService.makeAction(Action.GOLPE_ESTADO, socket.id, target);
+            GameService.makeAction(socket.id, Action.GOLPE_ESTADO, undefined, undefined, target, targetCard);
         });
 
         socket.on("trocarPropriaReligiao", () => {
-            GameService.makeAction(Action.TROCAR_PROPRIA_RELIGIAO, socket.id);
+            GameService.makeAction(socket.id, Action.TROCAR_PROPRIA_RELIGIAO);
         });
 
         socket.on("trocarReligiaoOutro", (targetName) => {
@@ -69,7 +69,7 @@ export default class GameService {
             if (target === null)
                 return;
 
-            GameService.makeAction(Action.TROCAR_RELIGIAO_OUTRO, socket.id, target);
+            GameService.makeAction(socket.id, Action.TROCAR_RELIGIAO_OUTRO, undefined, undefined, target);
         });
 
         socket.on("trocar", (card, selfCard, targetName, targetCard) => {
@@ -78,46 +78,46 @@ export default class GameService {
             if (target === null)
                 return;
 
-            GameService.makeAction(Action.TROCAR, socket.id, target);
+            GameService.makeAction(socket.id, Action.TROCAR, card, selfCard, target, targetCard);
         });
 
         socket.on("contestar", (card?, selfCard?) => {
-            GameService.makeAction(Action.CONTESTAR, socket.id);
+            GameService.makeAction(socket.id, Action.CONTESTAR, card, selfCard);
         });
 
         socket.on("bloquear", (card?, selfCard?) => {
-            GameService.makeAction(Action.BLOQUEAR, socket.id);
+            GameService.makeAction(socket.id, Action.BLOQUEAR, card, selfCard);
         });
 
         socket.on("continuar", () => {
-            GameService.makeAction(Action.CONTINUAR, socket.id);
+            GameService.makeAction(socket.id, Action.CONTINUAR);
         });
     }
 
-    static makeAction(action: Action, socketID: string, target?: Player) {
-        const turn = GameService.gatPlayersTurn(socketID);
+    static makeAction(
+        socketId: string,
+        action: Action,
+        card?: CardType,
+        selfCard?: number,
+        target?: Player,
+        targetCard?: number
+    ) {
+        const game = GameService.getPlayersGame(socketId);
 
-        if (turn === null)
+        if (game === null)
             return;
 
-        turn.addAction(action, target);
+        const player = PlayerService.getPlayer(socketId);
+
+        game.getTurn(player)?.addAction(action);
     }
 
-    static getPlayersGame(socketID: string): Game | null {
-        const lobby = PlayerService.getPlayersLobby(socketID);
+    static getPlayersGame(socketId: string): Game | null {
+        const lobby = PlayerService.getPlayersLobby(socketId);
 
         if (lobby === null)
             return null;
 
         return lobby.getGame();
-    }
-
-    static gatPlayersTurn(socketID: string): Turn | null {
-        const game = GameService.getPlayersGame(socketID);
-
-        if (game === null)
-            return null;
-
-        return game.getTurn(PlayerService.getPlayer(socketID));
     }
 }
