058b9097b5b72a7db3167179d0fc1e4f07bfc134
corrumptus <lazarinilucas42@gmail.com>
2024-08-14 16:43:43 -0300

    adding save ajuda externa

--- server/src/utils/ActionSaver.ts
+++ server/src/utils/ActionSaver.ts
@@ -14,7 +14,8 @@ export default class ActionSaver {
         const actionMapper: {
             [key in Action]: () => void;
         } = {
-            [Action.RENDA]: () => ActionSaver.saveRenda(turn, player, configs)
+            [Action.RENDA]: () => ActionSaver.saveRenda(turn, player, configs),
+            [Action.AJUDA_EXTERNA]: () => ActionSaver.saveAjudaExterna(turn, player, configs)
         }
 
         actionMapper[action]();
@@ -24,5 +25,11 @@ export default class ActionSaver {
         player.addMoney(configs.renda);
 
         turn.addAction(Action.RENDA);
+    }
+
+    private static saveAjudaExterna(turn: Turn, player: Player, configs: Config) {
+        player.addMoney(configs.ajudaExterna);
+
+        turn.addAction(Action.AJUDA_EXTERNA);
     }
 }
