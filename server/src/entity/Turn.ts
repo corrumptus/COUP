cd195845752a8e0580b11457303f7a4d2d000f83
corrumptus <lazarinilucas42@gmail.com>
2024-08-14 17:07:14 -0300

    adding new properties

--- server/src/entity/Turn.ts
+++ server/src/entity/Turn.ts
@@ -1,10 +1,13 @@
 import Action from "./Action";
+import CardType from "./CardType";
 import Player from "./player";
 
 export default class Turn {
     private player: Player;
     private target: Player | undefined;
     private actions: Action[];
+    private cardTypes: CardType[];
+    private cards: number[];
     private finished: boolean;
     private onFinish: () => void;
 
@@ -12,6 +15,8 @@ export default class Turn {
         this.player = player;
         this.target = undefined;
         this.actions = [];
+        this.cardTypes = [];
+        this.cards = [];
         this.finished = false;
         this.onFinish = onFinish;
     }
