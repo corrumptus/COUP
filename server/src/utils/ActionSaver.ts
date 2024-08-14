395d0196b0cf564dbf03939bfbc45be38d269aff
corrumptus <lazarinilucas42@gmail.com>
2024-08-14 16:33:58 -0300

    adding save renda

--- server/src/utils/ActionSaver.ts
+++ server/src/utils/ActionSaver.ts
@@ -1,12 +1,28 @@
 import Action from "../entity/Action";
+import Player from "../entity/player";
+import Turn from "../entity/Turn";
 import { ActionInfos } from "../service/GameMessageService";
+import Config from "./Config";
 
 export default class ActionSaver {
-    static save(action: Action): ActionInfos {
+    static save(
+        action: Action,
+        turn: Turn,
+        player: Player,
+        configs: Config
+    ): ActionInfos {
         const actionMapper: {
             [key in Action]: () => void;
-        } = {}
+        } = {
+            [Action.RENDA]: () => ActionSaver.saveRenda(turn, player, configs)
+        }
 
         actionMapper[action]();
+    }
+
+    private static saveRenda(turn: Turn, player: Player, configs: Config) {
+        player.addMoney(configs.renda);
+
+        turn.addAction(Action.RENDA);
     }
 }
