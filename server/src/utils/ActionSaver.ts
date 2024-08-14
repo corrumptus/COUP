7bd059c723338d98f060a1224f89c41641f9098b
corrumptus <lazarinilucas42@gmail.com>
2024-08-14 20:50:25 -0300

    adding save taxar

--- server/src/utils/ActionSaver.ts
+++ server/src/utils/ActionSaver.ts
@@ -1,4 +1,5 @@
 import Action from "../entity/Action";
+import CardType from "../entity/CardType";
 import Player from "../entity/player";
 import Turn from "../entity/Turn";
 import { ActionInfos } from "../service/GameMessageService";
@@ -9,13 +10,16 @@ export default class ActionSaver {
         action: Action,
         turn: Turn,
         player: Player,
+        cardType: CardType,
+        selfCard: number,
         configs: Config
     ): ActionInfos {
         const actionMapper: {
             [key in Action]: () => void;
         } = {
             [Action.RENDA]: () => ActionSaver.saveRenda(turn, player, configs),
-            [Action.AJUDA_EXTERNA]: () => ActionSaver.saveAjudaExterna(turn, player, configs)
+            [Action.AJUDA_EXTERNA]: () => ActionSaver.saveAjudaExterna(turn, player, configs),
+            [Action.TAXAR]: () => ActionSaver.saveTaxar(turn, player, cardType, selfCard, configs)
         }
 
         actionMapper[action]();
@@ -31,5 +35,19 @@ export default class ActionSaver {
         player.addMoney(configs.ajudaExterna);
 
         turn.addAction(Action.AJUDA_EXTERNA);
+    }
+
+    private static saveTaxar(
+        turn: Turn,
+        player: Player,
+        cardType: CardType,
+        selfCard: number,
+        configs: Config
+    ) {
+        player.addMoney(configs.tiposCartas[cardType].quantidadeTaxar);
+
+        turn.addAction(Action.TAXAR);
+        turn.addCardType(cardType);
+        turn.addCard(selfCard);
     }
 }
