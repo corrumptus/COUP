ac2556726ae82907a152b12ba74c2d1beb1a5fc6
corrumptus <lazarinilucas42@gmail.com>
2024-08-14 16:31:58 -0300

    initting the map pattern

--- server/src/utils/ActionSaver.ts
+++ server/src/utils/ActionSaver.ts
@@ -1,5 +1,12 @@
+import Action from "../entity/Action";
 import { ActionInfos } from "../service/GameMessageService";
 
 export default class ActionSaver {
-    static save(): ActionInfos {}
+    static save(action: Action): ActionInfos {
+        const actionMapper: {
+            [key in Action]: () => void;
+        } = {}
+
+        actionMapper[action]();
+    }
 }
