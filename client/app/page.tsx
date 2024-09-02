38f5220ded14adb5f910e76db95ae54712e4a136
corrumptus <lazarinilucas42@gmail.com>
2024-09-02 20:24:57 -0300

    change the layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,34 +4,11 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Header from "@pages/Header";
 import LobbysView from "@pages/LobbysView";
-import Toasters, { newToaster } from "@/utils/Toasters";
+import Toasters from "@/utils/Toasters";
 
 export default function Home() {
   const router = useRouter();
   const [ isServersVisible, setServersVisibility ] = useState(false);
-
-  async function createServer() {
-    if (localStorage.getItem("coup-token") === null) {
-      router.push("/sign-up");
-      return;
-    }
-
-    const response = await fetch("http://localhost:5000/lobby", {
-      headers: {
-        Authorization: localStorage.getItem("coup-token") as string
-      },
-      method: "POST"
-    });
-
-    const result: { error: string } | number = await response.json();
-
-    if (!response.ok) {
-      newToaster((result as { error: string }).error);
-      return;
-    }
-
-    router.push("/jogar/" + (result as number).toString());
-  }
 
   return (
     <div
@@ -43,8 +20,7 @@ export default function Home() {
         {isServersVisible &&
           <LobbysView closeView={() => setServersVisibility(false)}/>
         }
-        <button className="home_button" onClick={createServer}>Jogar</button>
-        <button className="home_button" onClick={() => setServersVisibility(true)}>Servidores</button>
+        <button className="home_button" onClick={() => setServersVisibility(true)}>Jogar</button>
         <button className="home_button" onClick={() => router.push("/regras")}>Regras</button>
         <button className="home_button" onClick={() => router.push("/tutorial")}>Tutorial</button>
         <button className="home_button" onClick={() => router.push("/stats")}>Estatísticas</button>
