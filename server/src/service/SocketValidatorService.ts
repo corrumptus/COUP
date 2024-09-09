8a9e0bc2803f0408247d1418e5383f551a03dec2
corrumptus <lazarinilucas42@gmail.com>
2024-09-08 21:26:54 -0300

    adding login validations

--- server/src/service/SocketValidatorService.ts
+++ server/src/service/SocketValidatorService.ts
@@ -2,6 +2,18 @@ import { COUPSocket } from "../socket/socket";
 
 export default class SocketValidatorService {
     static validate(socket: COUPSocket): string | undefined {
+        const auth = socket.handshake.auth;
+        if (
+            Object.keys(auth).length === 0
+            ||
+            (
+                !("token" in auth)
+                &&
+                !("name" in auth)
+            )
+        )
+            return "Usuário deve estar logado ou escolher um nome";
+
         return undefined;
     }
 }
