5208739dcf28b0e1789ea34decbe998e2f581586
corrumptus <lazarinilucas42@gmail.com>
2024-09-30 22:08:54 -0300

    implementing the rest of the turn finisher functions

--- server/src/utils/ActionTurnFinisher.ts
+++ server/src/utils/ActionTurnFinisher.ts
@@ -20,18 +20,18 @@ export default class ActionTurnFinisher {
         const actionMapper = {
             [Action.RENDA]: () => ActionTurnFinisher.finishRenda(turn),
             [Action.AJUDA_EXTERNA]: () => ActionTurnFinisher.finishAjudaExterna(lobbyId, game, turn),
-            [Action.TAXAR]: () => ActionTurnFinisher.finishTaxar(),
-            [Action.CORRUPCAO]: () => ActionTurnFinisher.finishCorrupcao(),
+            [Action.TAXAR]: () => ActionTurnFinisher.finishTaxar(lobbyId, game, turn),
+            [Action.CORRUPCAO]: () => ActionTurnFinisher.finishCorrupcao(lobbyId, game, turn),
             [Action.EXTORQUIR]: () => ActionTurnFinisher.finishExtorquir(),
             [Action.ASSASSINAR]: () => ActionTurnFinisher.finishAssassinar(),
             [Action.INVESTIGAR]: () => ActionTurnFinisher.finishInvestigar(),
-            [Action.GOLPE_ESTADO]: () => ActionTurnFinisher.finishGolpeEstado(),
-            [Action.TROCAR]: () => ActionTurnFinisher.finishTrocar(),
-            [Action.TROCAR_PROPRIA_RELIGIAO]: () => ActionTurnFinisher.finishTrocarPropriaReligiao(),
-            [Action.TROCAR_RELIGIAO_OUTRO]: () => ActionTurnFinisher.finishTrocarReligiaoOutro(),
+            [Action.GOLPE_ESTADO]: () => ActionTurnFinisher.finishGolpeEstado(turn),
+            [Action.TROCAR]: () => ActionTurnFinisher.finishTrocar(lobbyId, game, turn),
+            [Action.TROCAR_PROPRIA_RELIGIAO]: () => ActionTurnFinisher.finishTrocarPropriaReligiao(turn),
+            [Action.TROCAR_RELIGIAO_OUTRO]: () => ActionTurnFinisher.finishTrocarReligiaoOutro(turn),
             [Action.BLOQUEAR]: () => ActionTurnFinisher.finishBloquear(),
-            [Action.CONTESTAR]: () => ActionTurnFinisher.finishContestar(),
-            [Action.CONTINUAR]: () => ActionTurnFinisher.finishContinuar()
+            [Action.CONTESTAR]: () => ActionTurnFinisher.finishContestar(turn),
+            [Action.CONTINUAR]: () => ActionTurnFinisher.finishContinuar(turn)
         }
 
         actionMapper[action]();
@@ -45,5 +45,61 @@ export default class ActionTurnFinisher {
         ActionTurnFinisher.lobbys[lobbyId] = turn;
 
         game.nextPlayer();
+    }
+
+    private static finishTaxar(lobbyId: number, game: Game, turn: Turn) {
+        ActionTurnFinisher.lobbys[lobbyId] = turn;
+
+        game.nextPlayer();
+    }
+
+    private static finishCorrupcao(lobbyId: number, game: Game, turn: Turn) {
+        ActionTurnFinisher.lobbys[lobbyId] = turn;
+
+        game.nextPlayer();
+    }
+
+    private static finishExtorquir() {}
+
+    private static finishAssassinar() {}
+
+    private static finishInvestigar() {}
+
+    private static finishGolpeEstado(turn: Turn) {
+        turn.finish();
+    }
+
+    private static finishTrocar(lobbyId: number, game: Game, turn: Turn) {
+        const firstAction = turn.getFirstAction() as Action;
+
+        if (firstAction === Action.INVESTIGAR) {
+            turn.finish();
+            return;
+        }
+
+        ActionTurnFinisher.lobbys[lobbyId] = turn;
+
+        game.nextPlayer();
+    }
+
+    private static finishTrocarPropriaReligiao(turn: Turn) {
+        turn.finish();
+    }
+
+    private static finishTrocarReligiaoOutro(turn: Turn) {
+        turn.finish();
+    }
+
+    private static finishBloquear() {}
+
+    private static finishContestar(turn: Turn) {
+        turn.finish();
+    }
+
+    private static finishContinuar(turn: Turn) {
+        const firstAction = turn.getFirstAction() as Action;
+
+        if (firstAction !== Action.INVESTIGAR)
+            turn.finish();
     }
 }
