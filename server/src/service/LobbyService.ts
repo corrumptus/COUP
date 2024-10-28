c9c9bc8dceb5e86af3490dd2f5f0c190571763b0
corrumptus <lazarinilucas42@gmail.com>
2024-10-28 11:30:15 -0300

    fixing acessing a deleted info

--- server/src/service/LobbyService.ts
+++ server/src/service/LobbyService.ts
@@ -198,7 +198,7 @@ export default class LobbyService {
             LobbyMessageService.sendLobbyStateChanges(
                 lobbyId,
                 "newOwner",
-                (lobby.getOwner() as Player).name
+                player.name
             );
 
         if (!lobby.isEmpty)
