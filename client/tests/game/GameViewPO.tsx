21c1a14d02c63d279f9512b64579be61edfc90a8
corrumptus <lazarinilucas42@gmail.com>
2024-11-04 18:44:04 -0300

    upgrading the game view page object

--- client/tests/game/GameViewPO.tsx
+++ client/tests/game/GameViewPO.tsx
@@ -1,3 +1,27 @@
+import { render } from "@testing-library/react";
+import GameView from "@pages/GameView";
+import { GameState } from "@type/game";
+import { io, Socket } from "socket.io-client";
+import "./socketMock";
+
 export default class GameViewPO {
-  constructor() {}
+  private socket: Socket;
+
+  constructor(gameState: GameState, width: number = 800) {
+    this.socket = io();
+
+    Object.defineProperty(window, "screen", {
+      value: {
+        width: width
+      }
+    });
+
+    render(
+      <GameView
+        gameState={gameState}
+        socket={this.socket}
+        changeGameState={jest.fn()}
+      />
+    );
+  }
 }
