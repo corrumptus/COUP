4a399112bc9029c272b0722a018f388a0159f1da
corrumptus <lazarinilucas42@gmail.com>
2024-11-05 00:58:20 -0300

    creating a test for the game beginning

---
+++ client/tests/game/GameView.test.ts
@@ -0,0 +1,67 @@
+import { ContextType, GameState, PlayerState } from "@type/game";
+import GameViewPO from "./GameViewPO";
+import { faker } from "@faker-js/faker";
+import { randomCardType } from "./utils";
+import COUPDefaultConfigs from "@utils/COUPDefaultConfigs.json";
+
+jest.mock("next/navigation", () => ({
+    ...jest.requireActual("next/navigation"),
+    useRouter: jest.fn()
+}));
+
+describe("testing Game view game init render", () => {
+    const playerName = faker.person.fullName();
+    const gameInitState: GameState = {
+        player: {
+            name: playerName,
+            cards: [
+                {
+                    card: randomCardType(),
+                    isDead: false
+                },
+                {
+                    card: randomCardType(),
+                    isDead: false
+                }
+            ],
+            money: COUPDefaultConfigs.moedasIniciais,
+            state: PlayerState.WAITING_TURN
+        },
+        game: {
+            asylum: 0,
+            configs: COUPDefaultConfigs,
+            players: [
+                {
+                    name: faker.person.fullName(),
+                    cards: [
+                        {
+                            card: undefined,
+                            isDead: false
+                        },
+                        {
+                            card: undefined,
+                            isDead: false
+                        }
+                    ],
+                    money: COUPDefaultConfigs.moedasIniciais,
+                }
+            ],
+            currentPlayer: playerName,
+        },
+        context: {
+            type: ContextType.OBSERVING,
+            attacker: playerName,
+            isInvestigating: false
+        }
+    }
+
+    it("should render correctly in the game beginning when player is the first player and default configurations in pc view", async () => {
+        const gameView = new GameViewPO(gameInitState);
+
+        expect(gameView.html()).toBeInTheDocument();
+        expect(gameView.mobileMenuIcon()).not.toBeInTheDocument();
+        expect(gameView.nextPerson()).toBeInTheDocument();
+        expect(gameView.nextPerson()).toHaveTextContent(playerName);
+        expect(gameView.configDiff()).not.toBeInTheDocument();
+    });
+});
