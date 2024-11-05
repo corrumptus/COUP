8651aa728ac9b4ec1854310ed774a5e1f80a9db7
corrumptus <lazarinilucas42@gmail.com>
2024-11-05 00:58:59 -0300

    mocking the emit and the on methods

--- client/tests/game/socketMock.ts
+++ client/tests/game/socketMock.ts
@@ -1,8 +1,17 @@
+const socketEmitMock = jest.fn();
+const socketOnMock = jest.fn();
+
 jest.mock("socket.io-client", () => ({
     ...jest.requireActual("socket.io-client"),
     io: () => ({
-        emit: jest.fn(),
-        on: jest.fn(),
+        emit: socketEmitMock,
+        on: socketOnMock,
         removeAllListeners: jest.fn()
     })
 }));
+
+function getSocketOnEventHandler(type: string) {
+    const callback = socketOnMock.mock.calls.find(call => call[0] === type)[1];
+
+    return jest.fn(callback);
+}
