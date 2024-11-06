0dda7365dc1abe324ffb8b208f842e67f678f5b2
corrumptus <lazarinilucas42@gmail.com>
2024-11-06 18:36:35 -0300

    changing the creating of the game state to the factory

--- client/tests/game/GameView.test.ts
+++ client/tests/game/GameView.test.ts
@@ -1,8 +1,5 @@
-import { ContextType, GameState, PlayerState } from "@type/game";
 import GameViewPO from "./GameViewPO";
-import { faker } from "@faker-js/faker";
-import { randomCardType } from "./utils";
-import COUPDefaultConfigs from "@utils/COUPDefaultConfigs.json";
+import GameStateFactory from "./GameStateFactory";
 
 jest.mock("next/navigation", () => ({
     ...jest.requireActual("next/navigation"),
@@ -10,81 +7,36 @@ jest.mock("next/navigation", () => ({
 }));
 
 describe("testing Game view game init render", () => {
-    const playerName = faker.person.fullName();
-    const gameInitState: GameState = {
-        player: {
-            name: playerName,
-            cards: [
-                {
-                    card: randomCardType(),
-                    isDead: false
-                },
-                {
-                    card: randomCardType(),
-                    isDead: false
-                }
-            ],
-            money: COUPDefaultConfigs.moedasIniciais,
-            state: PlayerState.WAITING_TURN
-        },
-        game: {
-            asylum: 0,
-            configs: COUPDefaultConfigs,
-            players: [
-                {
-                    name: faker.person.fullName(),
-                    cards: [
-                        {
-                            card: undefined,
-                            isDead: false
-                        },
-                        {
-                            card: undefined,
-                            isDead: false
-                        }
-                    ],
-                    money: COUPDefaultConfigs.moedasIniciais,
-                }
-            ],
-            currentPlayer: playerName,
-        },
-        context: {
-            type: ContextType.OBSERVING,
-            attacker: playerName,
-            isInvestigating: false
-        }
-    }
-
     it("should render correctly in the game beginning when player is the first player and default configurations in pc view", async () => {
-        const gameView = new GameViewPO(gameInitState);
+        const gameState = new GameStateFactory().create();
+
+        const gameView = new GameViewPO(gameState);
 
         expect(gameView.html()).toBeInTheDocument();
         expect(gameView.mobileMenuIcon()).not.toBeInTheDocument();
         expect(gameView.nextPerson()).toBeInTheDocument();
-        expect(gameView.nextPerson()).toHaveTextContent(playerName);
+        expect(gameView.nextPerson()).toHaveTextContent(gameState.game.currentPlayer);
         expect(gameView.configDiffs()).not.toBeInTheDocument();
     });
 
     it("should render correctly in the game beginning when player is not the first player and default configurations in pc view", async () => {
-        const newGameState: GameState = JSON.parse(JSON.stringify(gameInitState));
+        const gameState = new GameStateFactory().toOtherPlayerTurn().create();
 
-        newGameState.game.currentPlayer = newGameState.game.players[0].name;
-
-        const gameView = new GameViewPO(newGameState);
+        const gameView = new GameViewPO(gameState);
 
         expect(gameView.html()).toBeInTheDocument();
         expect(gameView.mobileMenuIcon()).not.toBeInTheDocument();
         expect(gameView.nextPerson()).toBeInTheDocument();
-        expect(gameView.nextPerson()).toHaveTextContent(newGameState.game.players[0].name);
+        expect(gameView.nextPerson()).toHaveTextContent(gameState.game.players[0].name);
         expect(gameView.configDiffs()).not.toBeInTheDocument();
     });
 
     it("should render correctly in the game beginning when player is the first player and no default configurations in pc view", async () => {
-        const newGameState: GameState = JSON.parse(JSON.stringify(gameInitState));
-
-        newGameState.game.configs.moedasIniciais++;
-        newGameState.game.configs.renda++;
-        newGameState.game.configs.ajudaExterna--;
+        const gameState = new GameStateFactory()
+            .newConfig(["moedasIniciais"], (val: number) => val+1)
+            .newConfig(["renda"], (val: number) => val+1)
+            .newConfig(["ajudaExterna"], (val: number) => val-1)
+            .create();
 
         const expectedConfigDiffs = [
             "Moedas iniciais: 3 -> 4",
@@ -92,7 +44,7 @@ describe("testing Game view game init render", () => {
             "Ajuda externa: 2 -> 1"
         ];
 
-        const gameView = new GameViewPO(newGameState);
+        const gameView = new GameViewPO(gameState);
 
         expect(gameView.html()).toBeInTheDocument();
         expect(gameView.mobileMenuIcon()).not.toBeInTheDocument();
@@ -105,35 +57,35 @@ describe("testing Game view game init render", () => {
     });
 
     it("should render correctly in the game beginning when player is the first player and default configurations in mobile view", async () => {
-        const gameView = new GameViewPO(gameInitState, 500);
+        const gameState = new GameStateFactory().create();
+
+        const gameView = new GameViewPO(gameState, 500);
 
         expect(gameView.html()).toBeInTheDocument();
         expect(gameView.mobileMenuIcon()).toBeInTheDocument();
         expect(gameView.nextPerson()).toBeInTheDocument();
-        expect(gameView.nextPerson()).toHaveTextContent(playerName);
+        expect(gameView.nextPerson()).toHaveTextContent(gameState.game.currentPlayer);
         expect(gameView.configDiffs()).not.toBeInTheDocument();
     });
 
     it("should render correctly in the game beginning when player is not the first player and default configurations in mobile view", async () => {
-        const newGameState: GameState = JSON.parse(JSON.stringify(gameInitState));
+        const gameState = new GameStateFactory().toOtherPlayerTurn().create();
 
-        newGameState.game.currentPlayer = newGameState.game.players[0].name;
-
-        const gameView = new GameViewPO(newGameState, 500);
+        const gameView = new GameViewPO(gameState, 500);
 
         expect(gameView.html()).toBeInTheDocument();
         expect(gameView.mobileMenuIcon()).toBeInTheDocument();
         expect(gameView.nextPerson()).toBeInTheDocument();
-        expect(gameView.nextPerson()).toHaveTextContent(newGameState.game.players[0].name);
+        expect(gameView.nextPerson()).toHaveTextContent(gameState.game.players[0].name);
         expect(gameView.configDiffs()).not.toBeInTheDocument();
     });
 
     it("should render correctly in the game beginning when player is the first player and no default configurations in mobile view", async () => {
-        const newGameState: GameState = JSON.parse(JSON.stringify(gameInitState));
-
-        newGameState.game.configs.moedasIniciais++;
-        newGameState.game.configs.renda++;
-        newGameState.game.configs.ajudaExterna--;
+        const gameState = new GameStateFactory()
+            .newConfig(["moedasIniciais"], (val: number) => val+1)
+            .newConfig(["renda"], (val: number) => val+1)
+            .newConfig(["ajudaExterna"], (val: number) => val-1)
+            .create();
 
         const expectedConfigDiffs = [
             "Moedas iniciais: 3 -> 4",
@@ -141,7 +93,7 @@ describe("testing Game view game init render", () => {
             "Ajuda externa: 2 -> 1"
         ];
 
-        const gameView = new GameViewPO(newGameState, 500);
+        const gameView = new GameViewPO(gameState, 500);
 
         expect(gameView.html()).toBeInTheDocument();
         expect(gameView.mobileMenuIcon()).toBeInTheDocument();
