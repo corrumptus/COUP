43e3bc2ec7d28583358d663bc1fe9273a643702c
corrumptus <lazarinilucas42@gmail.com>
2024-11-08 14:19:53 -0300

    moving the socket mock to the test file

--- client/tests/game/GameView.test.ts
+++ client/tests/game/GameView.test.ts
@@ -1,12 +1,29 @@
 import { waitFor } from "@testing-library/dom";
 import GameViewPO from "./GameViewPO";
 import GameStateFactory from "./GameStateFactory";
-import { SocketEmitMock } from "./SocketMock";
 
 jest.mock("next/navigation", () => ({
     ...jest.requireActual("next/navigation"),
     useRouter: jest.fn()
 }));
+
+const socketEmitMock = jest.fn();
+const socketOnMock = jest.fn();
+
+jest.mock("socket.io-client", () => ({
+    ...jest.requireActual("socket.io-client"),
+    io: () => ({
+        emit: socketEmitMock,
+        on: socketOnMock,
+        removeAllListeners: jest.fn()
+    })
+}));
+
+function getSocketOnEventHandler(type: string) {
+    const callback = socketOnMock.mock.calls.find(call => call[0] === type)[1];
+
+    return jest.fn(callback);
+}
 
 describe("Game view render in game init", () => {
     it("should render correctly in the game beginning when player is the first player and default configurations in pc view", async () => {
