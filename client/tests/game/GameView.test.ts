bf2fb31144f3beb7e215a5aec43c62d39a09ff3e
corrumptus <lazarinilucas42@gmail.com>
2024-11-15 17:11:23 -0300

    adding tests for contestar after bloquear a ajuda externa

--- client/tests/game/GameView.test.ts
+++ client/tests/game/GameView.test.ts
@@ -977,6 +977,16 @@ describe("Game View render in game update", () => {
         expect(gameView.contestButton()).toBeInTheDocument();
         expect(gameView.acceptButton()).toBeInTheDocument();
     });
+
+    it("should render correctly when a player attack with bloquear after a ajuda externa", () => {
+        const { gameView } = initializeView(factory => factory
+            .ofBeingAttacked(Action.BLOQUEAR, Card.DUQUE, undefined, Action.AJUDA_EXTERNA)
+        );
+
+        expect(gameView.blockDefenseMenu()).toBeInTheDocument();
+        expect(gameView.contestButton()).toBeInTheDocument();
+        expect(gameView.acceptButton()).toBeInTheDocument();
+    });
 });
 
 describe("Game View interactivity in post game update when observing", () => {
@@ -1380,5 +1390,21 @@ describe("Game View interactivity in post game update when being attacked", () =
         expect(gameView.actionMenu()).not.toBeInTheDocument();
 
         expect(socketEmitMock).toHaveBeenCalledWith("contestar");
+    });
+
+    it("should render correctly when using contestar after blocking a ajuda externa", async () => {
+        const gameView = initializeView(factory => factory
+            .ofBeingAttacked(Action.BLOQUEAR, Card.DUQUE, undefined, Action.AJUDA_EXTERNA)
+        );
+
+        await gameView.contest();
+
+        expect(gameView.cardPickingMenu()).toBeInTheDocument();
+
+        await gameView.selectFirstPickableCard();
+
+        expect(gameView.actionMenu()).not.toBeInTheDocument();
+
+        expect(socketEmitMock).toHaveBeenCalledWith("contestar", 0);
     });
 });
