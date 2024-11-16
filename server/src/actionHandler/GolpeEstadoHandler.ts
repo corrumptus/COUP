cd42c1940f1b9e3917016f5996804fd2a17de93d
corrumptus <lazarinilucas42@gmail.com>
2024-11-16 20:04:14 -0300

    implementing golpe de estado handler

--- server/src/actionHandler/GolpeEstadoHandler.ts
+++ server/src/actionHandler/GolpeEstadoHandler.ts
@@ -1,23 +1,67 @@
-import CardType from "../entity/CardType";
+import Action from "../entity/Action";
 import Game from "../entity/Game";
-import { CardSlot } from "../entity/player";
+import Player, { CardSlot, isCardSlot } from "../entity/player";
 import { ActionInfos } from "../service/GameMessageService";
 import ActionHandler, { ActionRequest, ValidActionRequest } from "./ActionHandler";
 
 export default class GolpeEstadoHandler implements ActionHandler {
-    validate(request: ActionRequest): void {
-        throw new Error("Method not implemented.");
+    validate({
+        game,
+        player,
+        target,
+        targetCard
+    }: ActionRequest): void {
+        if (target === undefined)
+            throw new Error("Um inimigo deve ser escolhido");
+
+        if (targetCard === undefined)
+            throw new Error("Uma das cartas do inimigo deve ser escolhida");
+
+        if (!isCardSlot(targetCard))
+            throw new Error("O index da carta do inimigo deve ser 0 ou 1");
+
+        if (player.getMoney() < game.getConfigs().quantidadeMinimaGolpeEstado)
+            throw new Error("O player não tem dinheiro suficiente para dar um golpe de estado");
+
+        if (target.getCard(targetCard).getIsKilled())
+            throw new Error("A carta do inimigo escolhida já está morta");
     }
 
-    save(request: ValidActionRequest): boolean {
-        throw new Error("Method not implemented.");
+    save({
+        game,
+        player,
+        target,
+        targetCard
+    }: ValidActionRequest): boolean {
+        player.removeMoney(game.getConfigs().quantidadeMinimaGolpeEstado);
+
+        (target as Player).killCard(targetCard as CardSlot);
+
+        game.getLastTurn().addAction(Action.GOLPE_ESTADO);
+        game.getLastTurn().addTarget(target as Player);
+        game.getLastTurn().addCard(targetCard as CardSlot);
+
+        return true;
     }
 
-    finish(lobbyId: number, game: Game): void {
-        throw new Error("Method not implemented.");
+    finish(game: Game): boolean {
+        game.getLastTurn().finish();
+
+        return false;
     }
 
-    actionInfos(game: Game, card: CardType | undefined, targetCard: CardSlot | undefined): ActionInfos {
-        throw new Error("Method not implemented.");
+    actionInfos({
+        player,
+        target,
+        targetCard
+    }: ValidActionRequest): ActionInfos {
+        return {
+            attacker: player.name,
+            action: Action.GOLPE_ESTADO,
+            card: undefined,
+            target: (target as Player).name,
+            attackedCard: targetCard,
+            isInvestigating: false
+        }
     }
 }
