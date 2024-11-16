602a5be5584d9d6b1eaf807cba5350b96b9716ba
corrumptus <lazarinilucas42@gmail.com>
2024-11-16 18:40:57 -0300

    implementing extorquir handler

--- server/src/actionHandler/ExtorquirHandler.ts
+++ server/src/actionHandler/ExtorquirHandler.ts
@@ -1,23 +1,69 @@
+import Action from "../entity/Action";
 import CardType from "../entity/CardType";
-import Game from "../entity/Game";
-import { CardSlot } from "../entity/player";
+import Player, { CardSlot, isCardSlot } from "../entity/player";
 import { ActionInfos } from "../service/GameMessageService";
 import ActionHandler, { ActionRequest, ValidActionRequest } from "./ActionHandler";
 
 export default class ExtorquirHandler implements ActionHandler {
-    validate(request: ActionRequest): void {
-        throw new Error("Method not implemented.");
+    validate({
+        game,
+        player,
+        card,
+        selfCard,
+        target
+    }: ActionRequest): void {
+        if (card === undefined)
+            throw new Error("Um tipo de carta deve ser escolhido");
+
+        if (selfCard === undefined)
+            throw new Error("Uma das cartas do jogador deve ser escolhida");
+
+        if (!isCardSlot(selfCard))
+            throw new Error("O index da carta do jogador deve ser 0 ou 1");
+
+        if (target === undefined)
+            throw new Error("Um inimigo deve ser escolhido");
+
+        if (!game.getConfigs().tiposCartas[card].extorquir)
+            throw new Error("O tipo de carta escolhida não pode extorquir");
+
+        if (player.getCard(selfCard).getIsKilled())
+            throw new Error("A sua carta escolhida já está morta");
+
+        if (target.getMoney() < game.getConfigs().tiposCartas[card].quantidadeExtorquir)
+            throw new Error("O inimigo não tem dinheiro suficiente para ser extorquido");
     }
 
-    save(request: ValidActionRequest): boolean {
-        throw new Error("Method not implemented.");
+    save({
+        game,
+        card,
+        selfCard,
+        target
+    }: ValidActionRequest): boolean {
+        game.getLastTurn().addAction(Action.EXTORQUIR);
+        game.getLastTurn().addTarget(target as Player);
+        game.getLastTurn().addCardType(card as CardType);
+        game.getLastTurn().addCard(selfCard as CardSlot);
+
+        return true;
     }
 
-    finish(lobbyId: number, game: Game): void {
-        throw new Error("Method not implemented.");
+    finish(): boolean {
+        return false;
     }
 
-    actionInfos(game: Game, card: CardType | undefined, targetCard: CardSlot | undefined): ActionInfos {
-        throw new Error("Method not implemented.");
+    actionInfos({
+        player,
+        card,
+        target
+    }: ValidActionRequest): ActionInfos {
+        return {
+            attacker: player.name,
+            action: Action.EXTORQUIR,
+            card: card,
+            target: (target as Player).name,
+            attackedCard: undefined,
+            isInvestigating: false
+        };
     }
 }
