46d951ca29bcaa8a8358264660bfab0720983904
corrumptus <lazarinilucas42@gmail.com>
2024-11-16 15:58:24 -0300

    declaring all single class and adding to the factory

--- server/src/actionHandler/ActionHandlerFactory.ts
+++ server/src/actionHandler/ActionHandlerFactory.ts
@@ -1,8 +1,38 @@
 import Action from "../entity/Action";
 import ActionHandler from "./ActionHandler";
+import AjudaExternaHandler from "./AjudaExternaHandler";
+import AssassinarHandler from "./AssassinarHandler";
+import BloquearHandler from "./BloquearHandler";
+import ContestarHandler from "./ContestarHandler";
+import ContinuarHandler from "./ContinuarHandler";
+import CorrupcaoHandler from "./CorrupcaoHandler";
+import ExtorquirHandler from "./ExtorquirHandler";
+import GolpeEstadoHandler from "./GolpeEstadoHandler";
+import InvestigarHandler from "./InvestigarHandler";
+import RendaHandler from "./RendaHandler";
+import TaxarHandler from "./TaxarHandler";
+import TrocarHandler from "./TrocarHandler";
+import trocarPropriaReligiaoHandler from "./TrocarPropriaReligiao";
+import trocarReligiaoOutroHandler from "./TrocarReligiaoOutroHandler";
 
 export default class ActionHandlerFactory {
     static create(action: Action): ActionHandler {
-        throw new Error(`${action} doesnt have a action handler implementation`);
+        switch (action) {
+            case Action.RENDA: return new RendaHandler();
+            case Action.AJUDA_EXTERNA: return new AjudaExternaHandler();
+            case Action.TAXAR: return new TaxarHandler();
+            case Action.CORRUPCAO: return new CorrupcaoHandler();
+            case Action.EXTORQUIR: return new ExtorquirHandler();
+            case Action.ASSASSINAR: return new AssassinarHandler();
+            case Action.INVESTIGAR: return new InvestigarHandler();
+            case Action.GOLPE_ESTADO: return new GolpeEstadoHandler();
+            case Action.TROCAR: return new TrocarHandler();
+            case Action.TROCAR_PROPRIA_RELIGIAO: return new trocarPropriaReligiaoHandler();
+            case Action.TROCAR_RELIGIAO_OUTRO: return new trocarReligiaoOutroHandler();
+            case Action.BLOQUEAR: return new BloquearHandler();
+            case Action.CONTESTAR: return new ContestarHandler();
+            case Action.CONTINUAR: return new ContinuarHandler();
+            default: throw new Error(`${action} doesnt have a action handler implementation`);
+        }
     }
 }
---
+++ server/src/actionHandler/GolpeEstadoHandler.ts
@@ -0,0 +1,23 @@
+import CardType from "../entity/CardType";
+import Game from "../entity/Game";
+import { CardSlot } from "../entity/player";
+import { ActionInfos } from "../service/GameMessageService";
+import ActionHandler, { ActionRequest, ValidActionRequest } from "./ActionHandler";
+
+export default class GolpeEstadoHandler implements ActionHandler {
+    validate(request: ActionRequest): void {
+        throw new Error("Method not implemented.");
+    }
+
+    save(request: ValidActionRequest): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    finish(lobbyId: number, game: Game): void {
+        throw new Error("Method not implemented.");
+    }
+
+    actionInfos(game: Game, card: CardType | undefined, targetCard: CardSlot | undefined): ActionInfos {
+        throw new Error("Method not implemented.");
+    }
+}
