29b19ddb45b3cd62d21d1efc3f5e07206943ec0a
corrumptus <lazarinilucas42@gmail.com>
2024-11-16 19:56:01 -0300

    implementing investigar handler

--- server/src/actionHandler/InvestigarHandler.ts
+++ server/src/actionHandler/InvestigarHandler.ts
@@ -1,23 +1,79 @@
+import Action from "../entity/Action";
 import CardType from "../entity/CardType";
-import Game from "../entity/Game";
-import { CardSlot } from "../entity/player";
+import Player, { CardSlot, isCardSlot } from "../entity/player";
 import { ActionInfos } from "../service/GameMessageService";
 import ActionHandler, { ActionRequest, ValidActionRequest } from "./ActionHandler";
 
 export default class InvestigarHandler implements ActionHandler {
-    validate(request: ActionRequest): void {
-        throw new Error("Method not implemented.");
+    validate({
+        game,
+        player,
+        card,
+        selfCard,
+        target,
+        targetCard
+    }: ActionRequest): void {
+        if (card === undefined)
+            throw new Error("Um tipo de carta deve ser escolhido");
+
+        if (selfCard === undefined)
+            throw new Error("Uma das cartas do jogador deve ser escolhida");
+
+        if (!isCardSlot(selfCard))
+            throw new Error("O index da carta do jogador deve ser 0 ou 1");
+
+        if (target === undefined)
+            throw new Error("Um inimigo deve ser escolhido");
+
+        if (targetCard === undefined)
+            throw new Error("Uma das cartas do inimigo deve ser escolhida");
+
+        if (!isCardSlot(targetCard))
+            throw new Error("O index da carta do inimigo deve ser 0 ou 1");
+
+        if (!game.getConfigs().tiposCartas[card].investigar)
+            throw new Error("O tipo de carta escolhida não pode investigar");
+
+        if (player.getCard(selfCard).getIsKilled())
+            throw new Error("A sua carta escolhida já está morta");
+
+        if (target.getCard(targetCard).getIsKilled())
+            throw new Error("A carta do inimigo escolhida já está morta");
     }
 
-    save(request: ValidActionRequest): boolean {
-        throw new Error("Method not implemented.");
+    save({
+        game,
+        card,
+        selfCard,
+        target,
+        targetCard
+    }: ValidActionRequest): boolean {
+        game.getLastTurn().addAction(Action.ASSASSINAR);
+        game.getLastTurn().addTarget(target as Player);
+        game.getLastTurn().addCardType(card as CardType);
+        game.getLastTurn().addCard(selfCard as CardSlot);
+        game.getLastTurn().addCard(targetCard as CardSlot);
+
+        return true;
     }
 
-    finish(lobbyId: number, game: Game): void {
-        throw new Error("Method not implemented.");
+    finish(): boolean {
+        return true;
     }
 
-    actionInfos(game: Game, card: CardType | undefined, targetCard: CardSlot | undefined): ActionInfos {
-        throw new Error("Method not implemented.");
+    actionInfos({
+        player,
+        card,
+        target,
+        targetCard
+    }: ValidActionRequest): ActionInfos {
+        return {
+            attacker: player.name,
+            action: Action.INVESTIGAR,
+            card: card,
+            target: (target as Player).name,
+            attackedCard: targetCard,
+            isInvestigating: false
+        };
     }
 }
