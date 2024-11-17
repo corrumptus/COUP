5b2ba5cb4ca455e71cd741a56666a6c0d2c1c7b7
corrumptus <lazarinilucas42@gmail.com>
2024-11-17 07:34:38 -0300

    implementing trocar religiao outro handler

--- server/src/actionHandler/TrocarReligiaoOutroHandler.ts
+++ server/src/actionHandler/TrocarReligiaoOutroHandler.ts
@@ -1,23 +1,59 @@
-import CardType from "../entity/CardType";
+import Action from "../entity/Action";
 import Game from "../entity/Game";
-import { CardSlot } from "../entity/player";
+import Player from "../entity/player";
 import { ActionInfos } from "../service/GameMessageService";
 import ActionHandler, { ActionRequest, ValidActionRequest } from "./ActionHandler";
 
 export default class trocarReligiaoOutroHandler implements ActionHandler {
-    validate(request: ActionRequest): void {
-        throw new Error("Method not implemented.");
+    validate({
+        game,
+        player,
+        target
+    }: ActionRequest): void {
+        if (!game.getConfigs().religiao.reforma)
+            throw new Error("O jogo não passou pela reforma e não possui religião");
+
+        if (target === undefined)
+            throw new Error("Um inimigo deve ser escolhido");
+
+        if (!target.hasNonKilledCards)
+            throw new Error("O inimigo já está morto");
+
+        if (player.getMoney() < game.getConfigs().religiao.quantidadeTrocarOutro)
+            throw new Error("O player não tem dinheiro suficiente para trocar a religião do inimigo");
     }
 
-    save(request: ValidActionRequest): boolean {
-        throw new Error("Method not implemented.");
+    save({
+        game,
+        player,
+        target
+    }: ValidActionRequest): boolean {
+        player.removeMoney(game.getConfigs().religiao.quantidadeTrocarOutro);
+        (target as Player).changeReligion();
+
+        game.getLastTurn().addAction(Action.TROCAR_RELIGIAO_OUTRO);
+        game.getLastTurn().addTarget(target as Player);
+
+        return false;
     }
 
-    finish(lobbyId: number, game: Game): void {
-        throw new Error("Method not implemented.");
+    finish(game: Game): boolean {
+        game.getLastTurn().finish();
+
+        return false;
     }
 
-    actionInfos(game: Game, card: CardType | undefined, targetCard: CardSlot | undefined): ActionInfos {
-        throw new Error("Method not implemented.");
+    actionInfos({
+        player,
+        target
+    }: ValidActionRequest): ActionInfos {
+        return {
+            attacker: player.name,
+            action: Action.TROCAR_RELIGIAO_OUTRO,
+            card: undefined,
+            target: (target as Player).name,
+            attackedCard: undefined,
+            isInvestigating: false
+        };
     }
 }
