6c5b1cdafbdcc5c74736787b65c1f2565c966655
corrumptus <lazarinilucas42@gmail.com>
2024-11-29 02:43:46 -0300

    updating the dependencies

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: "/jogar/:id",
-            destination: "/jogar/:id"
-          },
-          {
-            source: '/:path*',
-            destination: '/',
-          },
-        ];
-    }
+  async rewrites() {
+    return [
+      {
+        source: "/jogar/:id",
+        destination: "/jogar/:id"
+      },
+      {
+        source: '/:path*',
+        destination: '/',
+      }
+    ];
+  }
 };
 
 export default nextConfig;
