b3f0993492d63789736f879cd490d494e2158e56
corrumptus <lazarinilucas42@gmail.com>
2024-12-01 02:01:41 -0300

    adding a test for renda

--- server/test/service/game.spec.ts
+++ server/test/service/game.spec.ts
@@ -1,1 +1,12 @@
-describe("game state in update", () => {});
+import GameClient from "./GameClient";
+
+describe("game state in update", () => {
+    it("should send the correct game state for renda", async () => {
+        const gameClient = await GameClient.create();
+
+        const expectedGameState = gameClient.firstDo("renda");
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith("updatePlayer", expectedGameState);
+    });
+});
