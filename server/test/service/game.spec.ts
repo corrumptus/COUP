60f872cff1233b18ecdb9f621b331302092944af
corrumptus <lazarinilucas42@gmail.com>
2024-12-04 10:02:12 -0300

    updating the renda test

--- server/test/service/game.spec.ts
+++ server/test/service/game.spec.ts
@@ -1,12 +1,26 @@
+import Action from "../../src/entity/Action";
 import GameClient from "./GameClient";
+import GameStateFactory from "./GameStateFactory";
 
 describe("game state in update", () => {
     it("should send the correct game state for renda", async () => {
         const gameClient = await GameClient.create();
 
-        const expectedGameState = gameClient.firstDo("renda");
+        gameClient.firstPlayerDo("renda");
 
         expect(gameClient.firstSocket().emit)
-            .toHaveBeenCalledWith("updatePlayer", expectedGameState);
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingSelf(Action.RENDA, undefined, false, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingEnemy(Action.RENDA, undefined, false, undefined, false)
+                    .create()
+            );
     });
 });
