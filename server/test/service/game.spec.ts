cb61e5b8ddf8df2838ddee8606cba13346450d90
corrumptus <lazarinilucas42@gmail.com>
2024-12-05 18:07:43 -0300

    adding tests for ajuda externa

--- server/test/service/game.spec.ts
+++ server/test/service/game.spec.ts
@@ -1,4 +1,5 @@
 import Action from "../../src/entity/Action";
+import CardType from "../../src/entity/CardType";
 import GameClient from "./GameClient";
 import GameStateFactory from "./GameStateFactory";
 
@@ -20,6 +21,79 @@ describe("game state in update", () => {
                 "updatePlayer",
                 new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
                     .ofSeeingEnemy(Action.RENDA, undefined, false, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for ajuda externa", async () => {
+        const gameClient = await GameClient.create();
+
+        gameClient.firstPlayerDo(Action.AJUDA_EXTERNA);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingSelf(Action.AJUDA_EXTERNA, undefined, false, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingEnemy(Action.AJUDA_EXTERNA, undefined, false, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using bloquear after a ajuda externa", async () => {
+        const gameClient = await GameClient.create();
+
+        gameClient.firstPlayerDo(Action.AJUDA_EXTERNA);
+
+        gameClient.clearMocks();
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofBeingAttacked(Action.BLOQUEAR, CardType.DUQUE, undefined, Action.AJUDA_EXTERNA)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingSelf(Action.BLOQUEAR, CardType.DUQUE, true, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using a contestar after using a bloquear after a ajuda externa", async () => {
+        const gameClient = await GameClient.create();
+
+        gameClient.firstPlayerDo(Action.AJUDA_EXTERNA);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        gameClient.clearMocks();
+
+        gameClient.firstPlayerDo(Action.CONTESTAR, 0);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingSelf(Action.CONTESTAR, undefined, true, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingEnemy(Action.CONTESTAR, undefined, true, undefined, false)
                     .create()
             );
     });
