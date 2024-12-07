23a2af05e92b42f189618cee458818d68be2a730
corrumptus <lazarinilucas42@gmail.com>
2024-12-07 20:03:04 -0300

    adding tests for taxar

--- server/test/service/game.test.ts
+++ server/test/service/game.test.ts
@@ -80,7 +80,7 @@ describe("game state in update", () => {
             );
     });
 
-    it("should send the correct game state for using bloquear after a ajuda externa", async () => {
+    it("should send the correct game state for using a continuar after a bloquear after a ajuda externa", async () => {
         const gameClient = await GameClient.create();
 
         gameClient.firstPlayerDo(Action.AJUDA_EXTERNA);
@@ -130,6 +130,137 @@ describe("game state in update", () => {
                 "updatePlayer",
                 new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
                     .ofSeeingEnemy(Action.CONTESTAR, undefined, true, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using a taxar", async () => {
+        const gameClient = await GameClient.create();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingSelf(Action.TAXAR, CardType.DUQUE, false, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingEnemy(Action.TAXAR, CardType.DUQUE, false, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using a bloquear after a taxar when a card can block it", async () => {
+        const gameClient = await GameClient.create([
+            [ ["tiposCartas", "duque", "bloquearTaxar"], true ]
+        ]);
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.clearMocks();
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofBeingAttacked(Action.BLOQUEAR, CardType.DUQUE, undefined, Action.TAXAR)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingSelf(Action.BLOQUEAR, CardType.DUQUE, true, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using a continuar after a bloquear after a taxar when a card can block it", async () => {
+        const gameClient = await GameClient.create([
+            [ ["tiposCartas", "duque", "bloquearTaxar"], true ]
+        ]);
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        gameClient.clearMocks();
+
+        gameClient.firstPlayerDo(Action.CONTINUAR);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingSelf(Action.CONTINUAR, undefined, false, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingEnemy(Action.CONTINUAR, undefined, false, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using a contestar after a bloquear after a taxar when a card can block it", async () => {
+        const gameClient = await GameClient.create([
+            [ ["tiposCartas", "duque", "bloquearTaxar"], true ]
+        ]);
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        gameClient.clearMocks();
+
+        gameClient.firstPlayerDo(Action.CONTESTAR);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingSelf(Action.CONTESTAR, undefined, true, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingEnemy(Action.CONTESTAR, undefined, true, undefined, false)
+                    .create()
+            );
+    });
+
+    it("should send the correct game state for using a contestar after a taxar", async () => {
+        const gameClient = await GameClient.create();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.clearMocks();
+
+        gameClient.secondPlayerDo(Action.CONTESTAR, 0);
+
+        expect(gameClient.firstSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.firstPlayer())
+                    .ofSeeingEnemy(Action.CONTESTAR, undefined, true, undefined, false)
+                    .create()
+            );
+        expect(gameClient.secondSocket().emit)
+            .toHaveBeenCalledWith(
+                "updatePlayer",
+                new GameStateFactory(gameClient.getGame(), gameClient.secondPlayer())
+                    .ofSeeingSelf(Action.CONTESTAR, undefined, true, undefined, false)
                     .create()
             );
     });
@@ -270,6 +401,192 @@ describe("game, turn and players state in update", () => {
         expect(gameClient.firstPlayer().getCard(0 as CardSlot).getIsKilled()).toBe(true);
         expect(gameClient.secondPlayer().getMoney()).toBe(3);
         expect(turn.getAllActions()).toStrictEqual([Action.AJUDA_EXTERNA, Action.BLOQUEAR, Action.CONTESTAR]);
+        expect(turn.getAllCards()).toStrictEqual([0, 0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).not.toStrictEqual(turn);
+
+        restoreMocks();
+    });
+
+    it("should update player money when using taxar", async () => {
+        const gameClient = await GameClient.create();
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(6);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR]);
+        expect(turn.getAllCards()).toStrictEqual([0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).not.toStrictEqual(turn);
+    });
+
+    it("should not update player money when using bloquear after taxar", async () => {
+        const gameClient = await GameClient.create([
+            [ [ "tiposCartas", "duque", "bloquearTaxar" ], true ]
+        ]);
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(6);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR, Action.BLOQUEAR]);
+        expect(turn.getAllCards()).toStrictEqual([0, 0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE, CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).toStrictEqual(turn);
+    });
+
+    it("should update player money back when using continuar after bloquear after taxar", async () => {
+        const gameClient = await GameClient.create([
+            [ [ "tiposCartas", "duque", "bloquearTaxar" ], true ]
+        ]);
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        gameClient.firstPlayerDo(Action.CONTINUAR);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(3);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR, Action.BLOQUEAR, Action.CONTINUAR]);
+        expect(turn.getAllCards()).toStrictEqual([0, 0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE, CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).not.toStrictEqual(turn);
+    });
+
+    it("should not update player money back when using contestar after bloquear after taxar", async () => {
+        const restoreMocks = GameClient.createMockImplementations([
+            CardType.ASSASSINO,
+            CardType.CAPITAO,
+            CardType.CONDESSA,
+            CardType.DUQUE
+        ]);
+
+        const gameClient = await GameClient.create([
+            [ [ "tiposCartas", "duque", "bloquearTaxar" ], true ]
+        ]);
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        gameClient.firstPlayerDo(Action.CONTESTAR);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(6);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(gameClient.secondPlayer().getCard(0 as CardSlot).getIsKilled()).toBe(true);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR, Action.BLOQUEAR, Action.CONTESTAR]);
+        expect(turn.getAllCards()).toStrictEqual([0, 0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE, CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).not.toStrictEqual(turn);
+
+        restoreMocks();
+    });
+
+    it("should =update player money back when using contestar after bloquear after taxar", async () => {
+        const restoreMocks = GameClient.createMockImplementations([
+            CardType.ASSASSINO,
+            CardType.CAPITAO,
+            CardType.DUQUE,
+            CardType.CONDESSA
+        ]);
+
+        const gameClient = await GameClient.create([
+            [ [ "tiposCartas", "duque", "bloquearTaxar" ], true ]
+        ]);
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.BLOQUEAR, CardType.DUQUE, 0);
+
+        gameClient.firstPlayerDo(Action.CONTESTAR);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(3);
+        expect(gameClient.firstPlayer().getCard(0 as CardSlot).getIsKilled()).toBe(true);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR, Action.BLOQUEAR, Action.CONTESTAR]);
+        expect(turn.getAllCards()).toStrictEqual([0, 0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE, CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).not.toStrictEqual(turn);
+
+        restoreMocks();
+    });
+
+    it("should not update player money back when using contestar after taxar", async () => {
+        const restoreMocks = GameClient.createMockImplementations([
+            CardType.DUQUE,
+            CardType.ASSASSINO,
+            CardType.CAPITAO,
+            CardType.CONDESSA
+        ]);
+
+        const gameClient = await GameClient.create();
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.CONTESTAR, 0);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(6);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(gameClient.secondPlayer().getCard(0 as CardSlot).getIsKilled()).toBe(true);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR, Action.CONTESTAR]);
+        expect(turn.getAllCards()).toStrictEqual([0, 0]);
+        expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE]);
+        expect(game.getAsylumCoins()).toBe(0);
+        expect(game.getLastTurn()).not.toStrictEqual(turn);
+
+        restoreMocks();
+    });
+
+    it("should update player money back when using contestar after taxar", async () => {
+        const restoreMocks = GameClient.createMockImplementations([
+            CardType.ASSASSINO,
+            CardType.CAPITAO,
+            CardType.CONDESSA,
+            CardType.DUQUE
+        ]);
+
+        const gameClient = await GameClient.create();
+
+        const game = gameClient.getGame();
+        const turn = game.getLastTurn();
+
+        gameClient.firstPlayerDo(Action.TAXAR, CardType.DUQUE, 0);
+
+        gameClient.secondPlayerDo(Action.CONTESTAR, 0);
+
+        expect(gameClient.firstPlayer().getMoney()).toBe(3);
+        expect(gameClient.secondPlayer().getCard(0 as CardSlot).getIsKilled()).toBe(true);
+        expect(gameClient.secondPlayer().getMoney()).toBe(3);
+        expect(gameClient.secondPlayer().getCard(0 as CardSlot).getIsKilled()).toBe(true);
+        expect(turn.getAllActions()).toStrictEqual([Action.TAXAR, Action.CONTESTAR]);
         expect(turn.getAllCards()).toStrictEqual([0, 0]);
         expect(turn.getAllCardTypes()).toStrictEqual([CardType.DUQUE]);
         expect(game.getAsylumCoins()).toBe(0);
