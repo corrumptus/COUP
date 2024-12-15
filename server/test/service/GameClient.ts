8500fa5911a26a39f6e19d6322a35f4e31674f82
corrumptus <lazarinilucas42@gmail.com>
2024-12-15 00:00:08 -0300

    updating propertys

--- server/test/service/GameClient.ts
+++ server/test/service/GameClient.ts
@@ -11,23 +11,20 @@ import type Player from "@entitys/player";
 import { createSocket, getSocketOnCB } from "@tests/utils";
 
 export default class GameClient {
+    private socket1: jest.Mocked<Socket>;
+    private socket2: jest.Mocked<Socket>;
     private player1: Player;
     private player2: Player;
     private game: Game;
-    private isFirstPlayerFirst: boolean;
-    private first: jest.Mocked<Socket>;
-    private second: jest.Mocked<Socket>;
 
     private constructor(socket1: jest.Mocked<Socket>, socket2: jest.Mocked<Socket>) {
+        this.socket1 = socket1;
+        this.socket2 = socket2;
+
         this.player1 = PlayerService.getPlayer(socket1.id);
         this.player2 = PlayerService.getPlayer(socket2.id);
+
         this.game = (LobbyService.getLobby(0) as Lobby).getGame() as Game;
-
-        const isPlayer1First = this.game.getLastTurn().getPlayer() === this.player1;
-
-        this.isFirstPlayerFirst = isPlayer1First;
-        this.first = isPlayer1First ? socket1 : socket2;
-        this.second = isPlayer1First ? socket2 : socket1;
 
         this.clearMocks();
     }
@@ -88,33 +85,33 @@ export default class GameClient {
     }
 
     firstSocket() {
-        return this.first;
+        return this.socket1;
     }
 
     firstPlayer() {
-        return this.isFirstPlayerFirst ? this.player1 : this.player2;
+        return this.player1;
     }
 
     secondSocket() {
-        return this.second;
+        return this.socket2;
     }
 
     secondPlayer() {
-        return this.isFirstPlayerFirst ? this.player2 : this.player1;
+        return this.player2;
     }
 
     firstPlayerDo<T extends Action>(action: T, ...args: Parameters<RequestSocketOnEvents[T]>) {
         // @ts-ignore
-        getSocketOnCB(this.first, action)(...args);
+        getSocketOnCB(this.socket1, action)(...args);
     }
 
     secondPlayerDo<T extends Action>(action: T, ...args: Parameters<RequestSocketOnEvents[T]>) {
         // @ts-ignore
-        getSocketOnCB(this.second, action)(...args);
+        getSocketOnCB(this.socket2, action)(...args);
     }
 
     clearMocks() {
-        this.first.emit.mockClear();
-        this.second.emit.mockClear();
+        this.socket1.emit.mockClear();
+        this.socket2.emit.mockClear();
     }
 }
