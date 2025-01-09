958eec9966bcb8170e8c455e8b88cd829a899a84
corrumptus <lazarinilucas42@gmail.com>
2025-01-09 13:42:15 -0300

    removing the buttons from the ui when the user is logged

--- client/components/home/Header.tsx
+++ client/components/home/Header.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image"
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const router = useRouter();
   const coupLogoLength = 50;
+  const [ signedIn, setSignedIn ] = useState(false);
+
+  useEffect(() => {
+    setSignedIn(localStorage.getItem("coup-token") !== null);
+  }, []);
 
   return (
     <header className="bg-[#eac58e] flex justify-between items-center p-2.5">
@@ -17,13 +23,15 @@ export default function Header() {
           onClick={() => router.push("/")}
         />
       </div>
-      <div className="flex gap-x-3">
-        <button onClick={() => router.push("/login")}>Entrar</button>
-        <button 
-          className="bg-sky-50 border-black border-2 rounded-3xl p-1.5 text-base font-bold"
-          onClick={() => router.push("/sign-up")}
-        >Inscrever-se</button>
-      </div>
+      {!signedIn &&
+        <div className="flex gap-x-3">
+          <button onClick={() => router.push("/login")}>Entrar</button>
+          <button 
+            className="bg-sky-50 border-black border-2 rounded-3xl p-1.5 text-base font-bold"
+            onClick={() => router.push("/sign-up")}
+          >Inscrever-se</button>
+        </div>
+      }
     </header>
   )
 }
